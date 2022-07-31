9ba11d2e370f1892519ae74d88098756aa24a27f
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-07-31 19:14:12 -0300

    create exercicios_secao_03/ex001.c

---
+++ introducao_a_linguagem_C/exercicios_secao_03/ex001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+   int valor;
+
+   printf("Informe um valor inteiro: ");
+   scanf("%d", &valor);
+
+   printf("O valor informado eh %d! \n", valor);
+   
+   return 0;
+}
