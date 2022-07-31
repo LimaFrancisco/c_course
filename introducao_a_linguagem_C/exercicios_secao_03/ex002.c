4d1390adb5fe89731def6a8f40e7b2f086761b7b
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-07-31 19:31:09 -0300

    create ex002.c

---
+++ introducao_a_linguagem_C/exercicios_secao_03/ex002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+   float valor;
+
+   printf("Informe um valor real: ");
+   scanf("%f", &valor);
+
+   printf("O valor real informado eh %f!\n", valor);
+
+   return 0;
+}
