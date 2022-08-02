3931ec1486844c343437beaf47ddf7a6fdaa4999
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-08-02 10:35:23 -0300

    create ex022.c

---
+++ 03_introducao_a_linguagem_C/exercicios_secao_03/ex022.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+   float jardas;
+   
+   printf("Informe um comprimento em jardas: \n");
+   scanf("%f", &jardas;
+
+   float metros = 0.91 * jardas;
+
+   printf("O comprimento informado convertido para metros eh: %f! \n", metros);
+
+   return 0;
+}
