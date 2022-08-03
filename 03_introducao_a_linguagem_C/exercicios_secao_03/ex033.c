b67299d1b9d5206a163cebb098d24058944ec5f9
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-08-03 11:13:20 -0300

    create ex033.c

---
+++ 03_introducao_a_linguagem_C/exercicios_secao_03/ex033.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+   int lado;
+
+   printf("Informe o lado do quadrado: \n");
+   scanf("%d", lado);
+
+   int area = lado * lado;
+
+   printf("A area do quadrado informado eh: %d\n", area);
+}
