8088f2256607407ba8476508c68da8effaf5e88b
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-08-25 10:41:59 -0300

    create instrucao_switch.c

---
+++ 04_estrutura_de_decisao_em_C/instrucao_switch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/*
+Switch
+
+- Usamos o switch em casos onde tenhamos uma estrutura grande ou confusa de if/else
+
+ */
+
+int main(){
+	
+	int valor;
+
+	printf("Digite um valor de 1 a 7:");
+	scanf("%d", &valor);
+
+	switch(valor){
+		case 1:
+			printf("Domingo\n");
+			break;
+		case 2:
+			printf("Segunda\n");
+			break;
+		case 3:
+			printf("Terca\n");
+			break;
+		case 4:
+			printf("Quarta\n");
+			break;
+		case 5:
+			printf("Quinta\n");
+			break;
+		case 6:
+			printf("Sexta\n");
+			break;
+		case 7:
+			printf("Sabado\n");
+			break;
+		default:
+			printf("Valor invalido\n");
+	}
+
+	return ;
+}
